3d8ed5bb521791f99c0c06a4bec19242dee26232
Lukas <`>
2020-07-15 12:30:19 +0300

    testing encoders
    testing encoders for different files encoding system

--- test.js
+++ test.js
@@ -1,28 +1,20 @@
-const replace = require('replace-in-file');
 
 
 
 
 
-async function findreplace () {
+const encoding = require('encoding-japanese');
+const fs = require('fs');
+const fileBuffer = fs.readFileSync('/mnt/d/CODE/watch_folder/master.ply');
 
-  const options = {
-    files: 'D:/CODE/watch folder/paste/*.ply',
-    encoding: 'utf16LE',
-    from: /"E:/g,
-    to: '"\\\\Pbneo-500-0719\\e\\'
-};
-
-  try {
-    const results = await replace(options)
-    console.log('Replacement results:', results);
-  }
-  catch (error) {
-    console.error('Error occurred:', error);
-  }
-}
- 
-findreplace();
+console.log(encoding.detect(fileBuffer))
 
 
- 
+
+var jschardet = require("jschardet")
+console.log(jschardet.detect(fileBuffer));
+
+
+
+
+// { encoding: "UTF-8", confidence: 0.9690625 }
